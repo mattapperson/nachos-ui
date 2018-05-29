71cd9f02f23055553fced0fe928de62595a5055c
Matt Apperson <me@mattapperson.com>
2018-05-29 09:01:58 -0400

    update tests/snapshots

--- src/Radio/Radio.spec.js
+++ src/Radio/Radio.spec.js
@@ -1,11 +1,15 @@
 import "react-native";
 import React from "react";
 import renderer from "react-test-renderer";
-
+import { Provider } from "../Theme";
 import Radio from "../Radio";
 
 it("should render correctly", () => {
-  const component = renderer.create(<Radio />);
+  const component = renderer.create(
+    <Provider>
+      <Radio />{" "}
+    </Provider>
+  );
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
