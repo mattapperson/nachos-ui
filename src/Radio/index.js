115dcfc9331b73f00b6dc12edb291bf983b3f11d
Matt Apperson <matthew.apperson@elastic.co>
2018-05-29 08:49:18 -0400

    update radio. Radio now uses theme

--- src/Radio/index.js
+++ src/Radio/index.js
@@ -1,9 +1,46 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Checkbox from "../Checkbox";
+import { withTheme } from "../Theme";
+import { View } from "react-native";
 
-const Radio = props => {
-  return <Checkbox {...props} />;
+const Radio = ({ theme, ...rest }) => {
+  return (
+    <Checkbox
+      checkComponent={<View style={[theme.check]} />}
+      theme={theme}
+      {...rest}
+    />
+  );
+};
+
+Radio.themeConfig = {
+  props: {
+    iconName: "md-checkmark",
+    iconColor: "#fff",
+    iconSize: 18
+  },
+  style: {
+    base: {
+      position: "relative",
+      width: 22,
+      height: 22,
+      borderWidth: 2,
+      justifyContent: "center",
+      alignItems: "center",
+      borderColor: "#ececee",
+      borderRadius: 9999
+    },
+    active: {
+      backgroundColor: "#fff"
+    },
+    check: {
+      width: 12,
+      height: 12,
+      borderRadius: 6,
+      backgroundColor: "@primaryColor"
+    }
+  }
 };
 
 Radio.propTypes = {
@@ -16,4 +53,4 @@ Radio.defaultProps = {
   kind: "circle"
 };
 
-export default Radio;
+export default withTheme("Radio", Radio);
