8e1f43d7a88809275914993fa9fb868058c12ce2
Matt Apperson <me@mattapperson.com>
2018-05-30 22:53:40 -0400

    cleanup

--- src/sketchapp-library/sketch-library.js
+++ src/sketchapp-library/sketch-library.js
@@ -21,7 +21,7 @@ import {
 
 const Document = ({ system }) => (
   <View>
-    <View name="Intro" style={{ width: 420, marginBottom: system.spacing * 4 }}>
+    <View name="Intro" style={{ width: 420, marginBottom: 34 }}>
       <Label>
         This is an example react-sketchapp document, showing how to render a
         styleguide from a data representation of your design system.
@@ -38,28 +38,17 @@ export function generateLibrary(context) {
   const document = createNewDocument(context);
 
   context.document = document;
-  console.log("here");
   renderLibrary(context);
-  console.log("here 2");
 
   const path = getStoragePath(context);
   saveDocument(document, path);
   closeDocument(document);
   addLibrary(path);
-  console.log("here 7");
 
   context.document.showMessage("Library added! 🙌");
 }
 
 export function renderLibrary(context) {
-  TextStyles.create(
-    {
-      context,
-      clearExistingStyles: true
-    },
-    designSystem.fonts
-  );
-
   //exposeSymbols();
 
   render(<Document system={designSystem} />, context.document.currentPage());
